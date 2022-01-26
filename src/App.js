251cb6f8fde6a73850f4dfdbf8b5af82bead734f
Clayton Knapp <csjknapp@gmail.com>
2022-01-26 14:31:38 -0800

    got lizard and alien working

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
 import './App.css';
-import VehicleList from './VehicleList';
-import TrafficLight from './TrafficLight';
+import { useState } from 'react';
+// import VehicleList from './VehicleList';
+// import TrafficLight from './TrafficLight';
 
 function App() {
   // track the following state with a few useState hooks:
-  // lightColor should be a string that starts out as 'red'
   // lizardSize should be a number that starts out as 10
+  const [lizardSize, setLizardSize] = useState(10);
+
   // alienSize should be a number that starts out as 10
+  const [alienSize, setAlienSize] = useState(10);
+
+  // lightColor should be a string that starts out as 'red'
   // traffic is complicated. It should be an array of strings that starts out as ['car', 'truck']
+
 
   return (
     <div className="App">
       <div className="fight">
         <div className="monster">
           {/* the width of the alien should be ten times whatever the alien size is in state */}
-          <img src="alien.png" width={20} />
+          <img src="alien.png" width={alienSize * 10} />
           <div className='buttons'>
             {/* when you click this button, the alien's size in state should go up by one */}
-            <button>Oh no! The alien is gobblin up all the electricity!</button>
+            <button 
+              onClick={ ()=> {
+                setAlienSize(alienSize + 1);
+              }}
+            >Oh no! The alien is gobbling up all the electricity!</button>
             {/* when you click this button, the lizard's size in state should go down by one */}
-            <button >Amazing! The alien zapped the lizard!</button>
+            <button 
+              onClick={ ()=> {
+                setLizardSize(lizardSize - 1);
+              }}
+            >Amazing! The alien zapped the lizard!</button>
           </div>
         </div>
         <div className="monster">
-          {/* the width of the lizard should be ten times whatever the alien size is in state */}
-          <img src="lizard.png" width={20} />
+          {/* the width of the lizard should be ten times whatever the lizard size is in state */}
+          <img src="lizard.png" width={lizardSize * 10} />
           <div className="buttons">
             {/* when you click this button, the lizard's size in state should go up by one */}
-            <button>Yegads! The lizard is ramping up to its final form!</button>
+            <button
+              onClick={ ()=> {
+                setLizardSize(lizardSize + 1);
+              }}
+            >Yegads! The lizard is ramping up to its final form!</button>
             {/* when you click this button, the alien's size in state should go up by one */}
-            <button>Oh my! The lizard chomped down on the alien!</button>
+            <button
+              onClick={ ()=> {
+                setAlienSize(alienSize - 1);
+              }}
+            >Oh my! The lizard chomped down on the alien!</button>
           </div>
         </div>
       </div>
-      <TrafficLight color={lightColor} />
+      {/* <TrafficLight color={lightColor} /> */}
       <div className="buttons">
         {/* when you click this button, the color of the light in state should be set to 'red' */}
         <button>Red</button>
@@ -47,7 +69,7 @@ function App() {
       This prop should be an array of strings like ['car', 'truck', 'truck', 'car', 'bus'].
       Do you have something like that in state that you could pass as a vehicles prop? 
       */}
-      <VehicleList />
+      {/* <VehicleList /> */}
       <div className='buttons'>
         {/* This part is weird */}
         {/* On click, you should set the traffic in state to be a copy of the same array that's already in state, but immutably add a 'car' to the end */}
