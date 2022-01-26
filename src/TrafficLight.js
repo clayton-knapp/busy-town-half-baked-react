50a2bdce22cf6c0efc870e3a2ef344affc99e191
Clayton Knapp <csjknapp@gmail.com>
2022-01-26 15:17:49 -0800

    got stop light working

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -1,16 +1,41 @@
 import React from 'react';
 
 export default function TrafficLight(props) {
+
+
+
   return (
     // this component takes in a prop called color. It is either 'red', 'yellow' or 'green'.
     <div className='traffic-light'>
       {/* in each of these divs, set it up so that if the color matches, add the css class 'on' to the classNames */}
+
+      {/* A SOLUTION I DONT LIKE - RENDERS ALL THREE */}
+      {/* {(props.color === 'red') && <div><div className={`red circle on`}></div><div className={`yellow circle`}></div><div className={`green circle`}></div></div>} */}
+
+
       {/* for example, the css classes for the div below should be `red circle on`, if props.color is 'red' */}
-      <div className={`red circle`}></div>
+      {/* ONE SOLUTION - CONDITIONAL RENDERING */}
+      <div className={
+        (props.color === 'red')
+          ? 'red circle on'
+          : 'red circle'
+      }></div>
+
+
       {/* the css classes for the div below should be `yellow circle on`, if props.color is 'yellow' */}
-      <div className={`yellow circle`}></div>
+      {/* SECOND SOLUTION - USES TEMPATE LITERAL -IS THIS OK? */}
+      <div className={`yellow circle ${
+        (props.color === 'yellow') && 'on'
+      }`}></div>
+
+
       {/*  the css classes for the div below should be `green circle on`, if props.color is 'green' */}
-      <div className={`green circle`}></div>
+      {/* THIRD SOLUTION - USES TERNARY TO RENDER WHOLE DIV */}
+      {
+        (props.color === 'green')
+          ? <div className={`green circle on`}></div>
+          : <div className={`green circle`}></div>
+      }
     </div>
   );
 }
